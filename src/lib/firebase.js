1d2eeb7e2492052457012404571dec72e673cdf6
Anmol Benipal <124711038+anmolbenipal@users.noreply.github.com>
2024-07-01 16:21:54 +0530

    Update firebase.js

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -1,15 +1,37 @@
+// import { initializeApp } from "firebase/app";
+// import { getAuth } from "firebase/auth";
+// import { getFirestore } from "firebase/firestore";
+// import { getStorage } from "firebase/storage";
+
+// const firebaseConfig = {
+//   apiKey: import.meta.env.VITE_API_KEY,
+//   authDomain: "reactchat-5b3d6.firebaseapp.com",
+//   projectId: "reactchat-5b3d6",
+//   storageBucket: "reactchat-5b3d6.appspot.com",
+//   messagingSenderId: "798876272902",
+//   appId: "1:798876272902:web:de21bf3eb6540dc890b687"
+// };
+
+// const app = initializeApp(firebaseConfig);
+
+// export const auth = getAuth()
+// export const db = getFirestore()
+// export const storage = getStorage()
+
+
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
+import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_API_KEY,
-  authDomain: "reactchat-5b3d6.firebaseapp.com",
-  projectId: "reactchat-5b3d6",
-  storageBucket: "reactchat-5b3d6.appspot.com",
-  messagingSenderId: "798876272902",
-  appId: "1:798876272902:web:de21bf3eb6540dc890b687"
+  apiKey: "AIzaSyCk3ry-jFWJbIkWxzWq_3s3GK9CFSOP9G4",
+  authDomain: "newchatapp-6ba47.firebaseapp.com",
+  projectId: "newchatapp-6ba47",
+  storageBucket: "newchatapp-6ba47.appspot.com",
+  messagingSenderId: "756585751069",
+  appId: "1:756585751069:web:2b26fd9fcfae9165bc9cf4",
+  measurementId: "G-GFE0L0FTTP"
 };
 
 const app = initializeApp(firebaseConfig);
