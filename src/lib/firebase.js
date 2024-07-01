69ca8f1f84039b0b2c63aa2d2cc0ba859c1c083d
Anmol Benipal <124711038+anmolbenipal@users.noreply.github.com>
2024-07-01 18:23:57 +0530

    Update firebase.js

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -1,17 +1,15 @@
-
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_API_KEY,
-  authDomain: "newchatapp-6ba47.firebaseapp.com",
-  projectId: "newchatapp-6ba47",
-  storageBucket: "newchatapp-6ba47.appspot.com",
-  messagingSenderId: "756585751069",
-  appId: "1:756585751069:web:2b26fd9fcfae9165bc9cf4",
-  measurementId: "G-GFE0L0FTTP"
+  authDomain: "reactchat-5b3d6.firebaseapp.com",
+  projectId: "reactchat-5b3d6",
+  storageBucket: "reactchat-5b3d6.appspot.com",
+  messagingSenderId: "798876272902",
+  appId: "1:798876272902:web:de21bf3eb6540dc890b687"
 };
 
 const app = initializeApp(firebaseConfig);
